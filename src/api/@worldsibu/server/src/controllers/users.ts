a2664728fe987be5c7a787cd0b2c00897602e12b
Jhoel Salas <fjhoelsg@users.noreply.github.com>
2018-10-28 01:43:35 -0600

    Extract handler definitions from users controller

--- src/api/@worldsibu/server/src/controllers/users.ts
+++ src/api/@worldsibu/server/src/controllers/users.ts
@@ -1,21 +1,6 @@
 import {Request, Response, Router} from 'express';
+import {RouterMethod} from './handler';
 import {UserStore} from '../store/user';
-
-type RequestHandler = (req: Request, res: Response) => void;
-
-function RouterMethod(handler: RequestHandler, self?): RequestHandler {
-  if (self) {
-    handler = handler.bind(self);
-  }
-  return async (req: Request, res: Response) => {
-    try {
-      await handler(req, res);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send(err);
-    }
-  };
-}
 
 export class UsersController {
 
